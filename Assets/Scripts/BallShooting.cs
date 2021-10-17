a398dd27f6f4fb0851631dfd957a212645119cb1
HurricaneDev1 <82486522+HurricaneDev1@users.noreply.github.com>
2021-10-16 19:21:46 -0700

    Pool Panic First commit

---
+++ Assets/Scripts/BallShooting.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallShooting : MonoBehaviour
+{
+    public Camera cam;
+    public Rigidbody2D rb;
+    Vector2 mousePos;
+    public Transform firePoint;
+    public GameObject cueBall;
+    public float ballSpeed = 20f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y,lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+
+    void Shoot()
+    {
+        GameObject ball = Instantiate(cueBall,firePoint.position,firePoint.rotation);
+        Rigidbody2D rig = ball.GetComponent<Rigidbody2D>();
+        rig.AddForce(firePoint.up * ballSpeed, ForceMode2D.Impulse);
+    }
+}
